e4e434604590f2a6dcba274c47c1e8fed2c17500
Dulanni01 <dulanimunoz01@gmail.com>
2026-09-19 09:37:31 -0600

    Agrega ruta /health

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send('Hola desde Docker');
 });
 
+app.get('/health',(req,res)=>{
+    res.send('OK');
+});
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
